91fa79d87e322034712cee8d0fe479f6da25440d
LearningGit <109271563+LerningGit@users.noreply.github.com>
2023-07-05 11:07:28 +0300

    Create ops_agent.tf

---
+++ ops_agent.tf
@@ -0,0 +1,27 @@
+module "agent_policy" {
+  source     = "terraform-google-modules/cloud-operations/google//modules/agent-policy"
+  version    = "~> 0.2.3"
+
+  project_id = "<PROJECT ID>"
+  policy_id  = "ops-agents-example-policy"
+  agent_rules = [
+    {
+      type               = "ops-agent"
+      version            = "current-major"
+      package_state      = "installed"
+      enable_autoupgrade = true
+    },
+  ]
+  group_labels = [
+    {
+      env = "prod"
+      app = "myproduct"
+    }
+  ]
+  os_types = [
+    {
+      short_name = "centos"
+      version    = "8"
+    },
+  ]
+}
